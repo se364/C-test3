5b3a80fa68c2f02788e20fb0547c15b5a030fd9c
Shah Emran <emranshah16@gmail.com>
2017-02-10 14:44:30 -0800

    Add files via upload

---
+++ question9.cpp
@@ -0,0 +1,40 @@
+/*Name : Shah Emran
+Date : 1/13/2017
+Homework 1
+CS&131         */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Code for question number 9
+
+	double a = 28.0;
+	double b = 32.0;
+	double c = 37.0;
+	double d = 24.0;
+	double e = 33.0;
+
+	double sum = a + b + c + d + e;  // sum of all the variables
+
+	double ave = 5.0;
+
+	double average = sum / ave;  // equation for the average of the variables
+
+
+	cout << "9)" << endl;
+	cout << "   The sum of the variables is " << sum << "\n";
+	cout << "   The average of the variable is " << average << "\n";
+
+	getchar();
+
+	return 0;
+
+
+
+
+
+
+}
