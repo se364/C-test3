bff8f815eb1e3b0960e6e836a425a4b5ec334516
Shah Emran <emranshah16@gmail.com>
2017-02-10 14:45:03 -0800

    Add files via upload

---
+++ question7.cpp
@@ -0,0 +1,32 @@
+/*Name : Shah Emran
+Date : 1/13/2017
+Homework 1
+CS&131         */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+
+	// Code for question number 7 
+	int  gas = 15;
+	int  distance = 375;
+
+	int  car_mpg = distance / gas;  // equation for mile per gallon
+
+
+	cout << "7) The number of miles per gallon the car gets is " << car_mpg << "\n";
+
+	getchar();
+
+	return 0;
+
+
+
+
+
+
+}
