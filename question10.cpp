cbd3509bd14a8c7c601dc25cec3dd5e7ff91127b
Shah Emran <emranshah16@gmail.com>
2017-02-10 14:44:00 -0800

    Add files via upload

---
+++ question10.cpp
@@ -0,0 +1,28 @@
+/*Name : Shah Emran
+Date : 1/13/2017
+Homework 1
+CS&131         */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	// Code for question number 10
+
+	cout << "10)" << endl;
+	cout << "   A character " << sizeof(char) << "byte of memory \n";   // displays number of bytes 
+	cout << "   A integer " << sizeof(int) << "byte of memory \n";
+	cout << "   A float " << sizeof(float) << "byte 0f memory \n";
+	cout << "   A double " << sizeof(double) << "byte of memory" << endl;
+
+
+	getchar();
+
+	return 0;
+
+
+
+}
