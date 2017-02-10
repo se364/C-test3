dccce4c469b2ec49f8a944f654fe717a1e6a32fd
Shah Emran <emranshah16@gmail.com>
2017-02-10 14:46:42 -0800

    Add files via upload

---
+++ quiz4question1.cpp
@@ -0,0 +1,42 @@
+/* Name: Shah Emran
+  Date : 1/19/2017
+  quiz 4
+  question 1*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b;    
+
+	// Entering the number 1 and 2
+	cout << "Enter number 1" << endl;
+	cin >> a;
+	cout << "Enter number 2" << endl;
+	cin >> b;
+
+
+	// condition statement for finding smaller and larger
+	if (a > b) {
+		cout << "number 1 is larger; number 2 is smaller" << endl;
+	}
+	else {
+		cout << "number 2 is larger; number 1 is smaller" << endl;
+	}
+
+	int x;
+	cin >> x;
+
+	return 0;
+
+     
+
+
+
+
+
+
+
+}
