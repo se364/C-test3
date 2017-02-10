6e1d4234d4dad8b65f11ae1c20b0ee7a29ab107c
Shah Emran <emranshah16@gmail.com>
2017-02-10 14:45:23 -0800

    Add files via upload

---
+++ question6.cpp
@@ -0,0 +1,32 @@
+/*Name : Shah Emran
+Date : 1/13/2017
+Homework 1
+CS&131         */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// This is question number 6
+	int x;
+	x = 50;
+	int y = 100;
+
+	int total;
+
+	total = x + y; // The total value
+
+	cout << "6) The sum of the two numbers is " << total << "\n";
+
+	getchar();
+
+	return 0;
+
+
+
+
+
+
+}
