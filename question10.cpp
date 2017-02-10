80627916fbe7b256f0d9f0204ebbe649db59a34b
Shah Emran <emranshah16@gmail.com>
2017-02-10 14:47:17 -0800

    Add files via upload

--- question10.cpp
+++ question10.cpp
@@ -1,28 +1,42 @@
-/*Name : Shah Emran
-Date : 1/13/2017
-Homework 1
-CS&131         */
-
-
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
 
 int main()
 {
+	    double a, b, c, d, e, average;
 
-	// Code for question number 10
+		cout << "Enter the first test score" << endl;
+		cin >> a;
+		cout << "Enter the second test score" << endl;
+		cin >> b;
+		cout << "Enter the third test score" << endl;
+		cin >> c;
+		cout << "Enter the fourth test score" << endl;
+		cin >> d;
+		cout << "Enter the fifth test score" << endl;
+		cin >> e;
 
-	cout << "10)" << endl;
-	cout << "   A character " << sizeof(char) << "byte of memory \n";   // displays number of bytes 
-	cout << "   A integer " << sizeof(int) << "byte of memory \n";
-	cout << "   A float " << sizeof(float) << "byte 0f memory \n";
-	cout << "   A double " << sizeof(double) << "byte of memory" << endl;
+		average = (a + b + c + d + e)/5;
+		
+		
+
+		//cout << "Test score 1 " << a << endl;
+		//cout << "Test score 2 " << b << endl;
+		//cout << "Test score 3 " << c << endl;
+		//cout << "Test score 4 " << d << endl;
+		//cout << "Test score 5 " << e << endl;
+		cout << "The average is  " << setprecision(1) << fixed << average;
+		cout << "\n";
+
+		int x;
+		cin >> x;
+
+		
 
 
-	getchar();
-
-	return 0;
-
+		return 0;
 
 
 }
